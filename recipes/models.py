381e59ff4288fcc34340c31fb15196e7c47dbef2
Ellen McCullagh <ellenmccullagh@gmail.com>
2017-08-25 20:53:42 -0700

    updated models to include unit, recipe ingredient and updated recipe model

--- recipes/models.py
+++ recipes/models.py
@@ -5,25 +5,73 @@ class Ingredient(models.Model):
     primary_name = models.CharField(
                                     max_length = 20,
                                     verbose_name = 'Primary Name',
+                                    unique = True
                                     )
-    # csv of alternate names for ingredient e.g. 00 flour,strong flour
-    # alternate_names = models.CharField(
-    #                                     max_length = 200,
-    #                                     verbose_name = 'Alternate Names',
-    #                                     blank = True,
-    #                                     null = True
-    #                                     )
 
     def __str__(self):
         return self.primary_name
 
-    def list_alternate_names(self):
-        #split csv of alternate names over , to return list of alternate names
-        return self.alternate_names.split(',')
+class Unit(models.Model):
+    UNIT_TYPE_CHOICES = (
+                            ('WGT', 'WEIGHT'),
+                            ('VOL', 'VOLUME'),
+                            ('CNT', 'COUNT')
+                        )
+    unit_type = models.CharField(max_length = 3, choices = UNIT_TYPE_CHOICES, default = 'CNT')
+
+    unit_name = models.CharField(max_length = 20)
+    unit_name_plural = models.CharField(max_length = 21)
+    unit_abbreviation = models.CharField(max_length = 5)
+    unit_abbreviation_plural = models.CharField(max_length = 6)
+
+    # def alternate_forms(self, abbreviation = True, plural = True):
+    #     units_all_forms = [
+    #                         ['LBS', 'POUND', 'POUNDS', 'LB', 'LBS'],
+    #                         ['KGS', 'KILOGRAM', 'KILOGRAMS', 'KG', 'KG'],
+    #                         ['GRA', 'GRAM', 'GRAMS', 'G', 'G'],
+    #                         ['OZS', 'OUNCE', 'OUNCES', 'OZ', 'OZ'],
+    #                         ['LIT', 'LITER', 'LITERS', 'L', 'L'],
+    #                         ['MLS', 'MILILITER', 'MILILITERS', 'ML', 'ML'],
+    #                         ['CUP', 'CUP', 'CUPS', 'C', 'C'],
+    #                         ['TSP', 'TEASPOON', 'TEASPOONS', 'TSP', 'TSP'],
+    #                         ['TBS', 'TABLESPOON', 'TABLESPOONS', 'TBSP', 'TBSP'],
+    #                         ['PIN', 'PINT', 'PINTS', 'PT', 'PT'],
+    #                         ['QRT', 'QUART', 'QUARTS', 'QT', 'QT'],
+    #                         ['GAL', 'GALLON', 'GALLONS', 'GAL', 'GAL'],
+    #                         ['FLZ', 'FLUID OUNCE', 'FLUID OUNCES', 'FL OZ', 'FL OZ'],
+    #                         ['PCH', 'PINCH', 'PINCHES', 'PINCH', 'PINCHES'],
+    #                         ['DSH', 'DASH', 'DASHES', 'DASH', 'DASHES'],
+    #                         ['CNT', 'COUNT', 'COUNT', 'COUNT', 'COUNT']
+    #                     ]
+    #     for unit_forms in units_all_forms:
+    #         if self.unit == unit_forms[0]:
+    #             if not abbreviation and not plural:
+    #                 return unit_forms[1]
+    #             else if not abbreviation and plural:
+    #                 return unit_forms[2]
+    #             else if abbreviation and not plural:
+    #                 return unit_forms[3]
+    #             else if abbreviation and plural:
+    #                 return unit_forms[4]
+
+    def __str__(self):
+        return self.unit_name
+
+class RecipeIngredient(models.Model):
+    recipe_text = models.CharField(max_length = 50)
+    matched_ingredient = models.ForeignKey(Ingredient)
+    unit = models.ForeignKey(Unit)
+    ammount = models.DecimalField(max_digits = 4, decimal_places = 3)
+    associated_recipe_slug = models.CharField(max_length = 40)
+
+    def __str__(self):
+        return self.recipe_text
 
 class Recipe(models.Model):
-    #recipe title
-    title = models.CharField(max_length = 40)
+    #recipe title and  derived url slug
+    title = models.CharField(max_length = 40, unique = True)
+    slug = models.SlugField(max_length = 40, unique = True)
+    prepopulated_fields = {'slug': {'title', }}
     #recipe source (e.g. NYT, Bon Appetit)
     source = models.CharField(max_length = 100)
     #recipe source url, note this is not required since some recipes will not have a source url
@@ -39,9 +87,7 @@ class Recipe(models.Model):
     #time to cook recipe in minutes
     cook_time = models.PositiveIntegerField(blank = True, null = True)
     #list of ingredients (many to many field)
-    ingredients = models.ManyToManyField(Ingredient)
-    #csv with ingredient primary name and quantity deliminated as follows: flour,100g;water,50g;salt,1tsp
-    ingredient_list = models.CharField(max_length = 500)
+    ingredient_list = models.ManyToMany(RecipeIngredient)
     #instruction list - determine format later
     instructions = models.TextField()
     #meal type (choices: breakfast, lunch, dinner, dessert, snack)
@@ -63,3 +109,6 @@ class Recipe(models.Model):
     )
     dish_type = models.CharField(max_length = 3, choices = DISH_TYPE_CHOICES, default = 'VEG')
     #flavor profile helps create meals that go together
+
+        def __str__(self):
+            return self.title
